42eee91c85b89e8c6dab6037087531f01baab3ef
sysel <vojtech.sysel@email.cz>
2015-01-10 16:03:45 +0100

    Initial version of Nette Component

---
+++ PHP/tasks/Task/Nette/Component.php
@@ -0,0 +1,84 @@
+<?php
+namespace Task\Nette;
+
+use Task,
+	Nette;
+
+/**
+ * Task component for Nette
+ */
+class Component extends Nette\Application\UI\Control
+{
+	/** @var callback[] $onStepEvent Callback for each step */
+	public $onStepEvent = array();
+
+	/** @var callback[] $onFinishEvent Callback after finish */
+	public $onFinishEvent = array();
+
+	/** @var Task\ITaskRunner $runner Task runner */
+	protected $runner;
+
+	/**
+	 * @param Task\ITaskRunner Task runner
+	 * @param Nette\ComponentModel\IContainer Parent component
+	 * @param string Component name
+	 */
+	public function __construct(Task\ITaskRunner $runner, Nette\ComponentModel\IContainer $parent = NULL, $name = NULL) {
+		parent::__construct($parent, $name);
+
+		$this->runner = $runner;
+	}
+
+	/**
+	 * Handlers
+	 */
+
+	/**
+	 * Step handler
+	 */
+	public function handleStep() {
+		if (!$this->runner->isFinished()) {
+			$this->runner->run();
+		}
+
+		if ($this->getPresenter()->isAjax()) {
+			$this->invalidateControl('taskComponent');
+		}
+	}
+
+	/**
+	 * Component render
+	 */
+
+	/**
+	 * Render component
+	 */
+	public function render() {
+		$template = $this->template;
+		$template->setFile(__DIR__ . '/taskComponent.latte');
+
+		$progress = $this->runner->getProgress();
+		$template->inProgress = $progress['remain'] !== 0;
+		$template->progressPercentage = round(($progress['done'] / ($progress['count'] == 0 ? 1 : $progress['count'])) * 100);
+		$template->componentName = $this->getName();
+
+		$template->render();
+
+		// finish event
+		if ($progress['remain'] === 0) {
+			foreach ($this->onFinishEvent as $handler) {
+				Nette\Utils\Callback::invoke($handler);
+			}
+		}
+	}
+
+	/**
+	 * Return component to string
+	 * @return string Rendered component
+	 */
+	public function __toString() {
+		ob_start();
+		$this->render();
+		return ob_get_clean();
+	}
+}
